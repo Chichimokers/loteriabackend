93677869dfaa7dd7bd53b421a92c837f420937ae
Chichimokers <developer1575@gmail.com>
2026-03-23 01:18:35 -0400

    aaa

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
     UsuarioViewSet, TarjetaSistemaViewSet,
     SolicitudAcreditacionViewSet, ExtraccionViewSet
 )
 
 router = DefaultRouter()
-router.register(r'usuarios', UsuarioViewSet, basename='usuarios')
+router.register(r'', UsuarioViewSet, basename='usuarios')
 router.register(r'tarjetas', TarjetaSistemaViewSet, basename='tarjetas')
 router.register(r'acreditaciones', SolicitudAcreditacionViewSet, basename='acreditaciones')
 router.register(r'extracciones', ExtraccionViewSet, basename='extracciones')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
