108b3e21088cfdb3ac563872d8289ad9c2345192
Chichimokers <developer1575@gmail.com>
2026-03-25 16:11:36 -0400

    feat: mejorar admin para editar saldo de usuarios
    - Agregado list_editable para saldo_principal y saldo_extraccion
    - Agregado fieldsets para organizar el formulario
    - Agregadas acciones personalizadas: agregar_saldo y restar_saldo (100 CUP)

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from .models import Usuario, TarjetaSistema, SolicitudAcreditacion, Extraccion
 
 
@@ -8,6 +9,44 @@ class UsuarioAdmin(admin.ModelAdmin):
     list_filter = ['banco', 'fecha_registro']
     search_fields = ['email', 'movil']
     readonly_fields = ['fecha_registro', 'password']
+    list_editable = ['saldo_principal', 'saldo_extraccion']
+    
+    fieldsets = (
+        ('Información de cuenta', {
+            'fields': ('email', 'username', 'password', 'movil')
+        }),
+        ('Información bancaria', {
+            'fields': ('tarjeta_bancaria', 'banco')
+        }),
+        ('Saldos', {
+            'fields': ('saldo_principal', 'saldo_extraccion')
+        }),
+        ('Permisos', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Fechas', {
+            'fields': ('fecha_registro',)
+        }),
+    )
+    
+    actions = ['agregar_saldo', 'restar_saldo']
+    
+    @admin.action(description='Agregar 100 CUP al saldo principal')
+    def agregar_saldo(self, request, queryset):
+        for usuario in queryset:
+            usuario.saldo_principal += 100
+            usuario.save()
+        self.message_user(request, f'Se agregó 100 CUP a {queryset.count()} usuario(s)', messages.SUCCESS)
+    
+    @admin.action(description='Restar 100 CUP al saldo principal')
+    def restar_saldo(self, request, queryset):
+        for usuario in queryset:
+            if usuario.saldo_principal >= 100:
+                usuario.saldo_principal -= 100
+                usuario.save()
+            else:
+                self.message_user(request, f'El usuario {usuario.email} no tiene saldo suficiente', messages.WARNING)
+        self.message_user(request, f'Se restó 100 CUP a los usuarios con saldo suficiente', messages.SUCCESS)
 
 
 @admin.register(TarjetaSistema)
